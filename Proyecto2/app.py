f9ef54c6cbfd3cc488e46ab44853ff6fefb3b9fb
Sebastian Chu <45053945+ChuSebastian@users.noreply.github.com>
2024-11-30 21:17:48 -0500

    flask app backend terminado

--- Proyecto2/app.py
+++ Proyecto2/app.py
@@ -2,21 +2,21 @@ from flask import Flask, render_template, request
 from Spimi import SPIMIIndex
 import pandas as pd
 import psycopg2
+import time  # Para medir el tiempo
 
 app = Flask(__name__)
 
-# Configuración
+# Configuración para el SPIMI
 chunk_size = 10000
 memory_limit = 500000
 index = SPIMIIndex(memory_limit=memory_limit)
 
-# Cargar el archivo CSV
 try:
     df = pd.read_csv("data.csv")
 except Exception as e:
     df = None
 
-# Configuración de PostgreSQL
+# Configuración de PostgreSQL --> modificar dbname y password para probar
 db_config = {
     'dbname': 'dbname',
     'user': 'postgres',
@@ -27,7 +27,8 @@ db_config = {
 
 @app.route("/", methods=["GET", "POST"])
 def search():
-    results = []
+    results_df = pd.DataFrame()  
+    query_time = None  
     if request.method == "POST":
         query = request.form.get("query")  
         topK = request.form.get("topK")  
@@ -41,9 +42,19 @@ def search():
             if df is None:
                 raise ValueError("El archivo CSV no se pudo cargar.")
 
+            start_time = time.time()
+
             if source == "SPIMI":
                 query_index = index.retrieve_index(query)
-                results = [df.loc[doc_id, "merge"] for doc_id in list(query_index.keys())[:topK]]
+                data = dict(list(query_index.items())[:topK])
+
+                results = []
+                for doc_id, score in data.items():
+                    merge_value = df.loc[doc_id, "merge"]
+                    results.append({"merge": merge_value, "score": score})
+                
+                results_df = pd.DataFrame(results)
+
             elif source == "Postgres":
                 query_string = " | ".join(query.split())
                 with psycopg2.connect(**db_config) as conn:
@@ -57,13 +68,20 @@ def search():
                             """, 
                             (query_string, query_string, topK)
                         )
-                        results = [row[0] for row in cur.fetchall()]
+                        results = [{"merge": row[0], "score": row[1]} for row in cur.fetchall()]
+                
+                results_df = pd.DataFrame(results)
+
             else:
                 raise ValueError("Fuente de búsqueda desconocida.")
+
+            end_time = time.time()
+            query_time = round(end_time - start_time, 3)
+
         except Exception as e:
             results = [f"Error: {str(e)}"]
 
-    return render_template("index.html", results=results)
+    return render_template("index.html", results_df=results_df.to_dict(orient="records"), query_time=query_time)
 
 
 if __name__ == "__main__":
