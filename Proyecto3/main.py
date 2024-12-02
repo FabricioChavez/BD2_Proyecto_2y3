2fc2828cf6030868c56dbd576809bc5d8554f874
paolomedrano04 <paolo.medrano@utec.edu.pe>
2024-12-01 19:03:05 -0500

    tiempos/rango

--- Proyecto3/main.py
+++ Proyecto3/main.py
@@ -4,11 +4,12 @@ import sys
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(project_root, 'src'))
 
-from Experiments.Time_extractor_KnnSeq import run_knn_seq_experiment
-from Experiments.Time_extractor_KnnLsh import run_knn_lsh_experiment
-from Experiments.Time_extractor_KnnIvf import run_knn_ivf_experiment
-
+#from Experiments.Time_extractor_KnnSeq import run_knn_seq_experiment
+#from Experiments.Time_extractor_KnnLsh import run_knn_lsh_experiment
+#from Experiments.Time_extractor_KnnIvf import run_knn_ivf_experiment
+from Experiments.time_extractor_KnnSeqRango import run_knn_seq_ran_experiment
 if __name__ == "__main__":
     #run_knn_seq_experiment()
     #run_knn_lsh_experiment()
-    run_knn_ivf_experiment()
+   # run_knn_ivf_experiment()
+   run_knn_seq_ran_experiment()
